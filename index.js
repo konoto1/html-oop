ddda49a02f96b6c6b56962e04e8307072b122f92
Tomas.K <tomas.konopackis@gmail.com>
2024-07-19 13:55:20 +0300

    update

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ Metodai:
 
 console.clear();
 
-const Automobilis = new Car('Audi', '80', 'Raudona', '68ltr', '7.6ltr');
+const Automobilis = new Car('Audi', '80', 'Raudona', '68ltr', '7.6ltr/100km');
 
 console.log(Automobilis);
 console.log(Automobilis.ijungtiVarikli());
@@ -84,6 +84,7 @@ console.log(Automobilis.kiekLikoKuro());
 console.log(Automobilis.sustoti());
 console.log(Automobilis.isjungtiVarikli());
 console.log(Automobilis.uzpiltiKuro());
+console.log(Automobilis);
 
 
 

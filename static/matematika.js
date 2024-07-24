3225bd1114bd6f957c396bf348a999ba69b5cfcd
Tomas.K <tomas.konopackis@gmail.com>
2024-07-24 12:38:36 +0300

    funkcijos pasleptos po objekto raktais

---
+++ static/matematika.js
@@ -0,0 +1,10 @@
+
+export const matematika = {
+    add: (a, b) => a + b,
+    diff: (a, b) => a - b,
+    multi: (a, b) => a * b,
+    div: (a, b) => a / b,
+    mod: (a, b) => a % b,
+};
+
+console.log(matematika.add(11, 19));
